f586b1f22b58afd690f6ea44798dc428813e751d
cstnn <35491404+cstnn@users.noreply.github.com>
2021-12-23 16:25:14 +0200

    Update _gmail.py

--- _gmail.py
+++ _gmail.py
@@ -7,10 +7,9 @@ def send_email(subject, email_text, email_to):
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     PORT = 465  # For SSL
     HOST = "smtp.gmail.com"
-    FROM = "costin.nadolu@gmail.com"  # Enter your address
-    PASSWORD = "dgubzcqbhxxasxtv"
+    FROM = "yourname@gmail.com"  # Enter your address
+    PASSWORD = "yourpassword"
 
-    # TO = ['costin.nadolu@gmail.com', 'razvan.ciocoiu19@yahoo.com'] #'andreea_nadolu@yahoo.com',
     TO = email_to
     SUBJECT = subject
         
@@ -25,4 +24,4 @@ def send_email(subject, email_text, email_to):
         server.sendmail(FROM, TO, MESSAGE)
 
 if __name__ == "__main__":
-    send_email("subject", "email_text", "costin.nadolu@gmail.com")
+    send_email("subject", "email_text", "yourname@gmail.com")
